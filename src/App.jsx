46c7b9e68a0da08fbf406ade96e55ca317ccb7bd
mrasyidmrd <mrasyidmrd@gmail.com>
2026-02-16 09:06:04 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -172,7 +172,15 @@ const getTimestamp = () => {
 
     <tr key={i}>
       <td>{i + 1}</td> {/* Nomor otomatis */}
-      <td>{t.tanggal}</td>
+      <td>
+		{new Date(item.tanggal).toLocaleString("id-ID", {
+			day: "2-digit",
+			month: "long",
+			year: "numeric",
+			hour: "2-digit",
+			minute: "2-digit"
+		  })}  
+	  </td>
       <td>{t.jenisTransaksi}</td>
       <td>{t.keterangan || "-"}</td>
       <td>Rp {t.nominal.toLocaleString("id-ID")}</td>
